e69bd6a6347481e03e6aeab958074389a9184427
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-03-08 16:46:32 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,27 @@
+import asyncio
 
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from config import BOT_TOKEN
+from handlers import router
+from database import init_db, seed_gifts
+
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+
+    dp = Dispatcher(storage=MemoryStorage())
+
+    dp.include_router(router)
+
+    await init_db()
+    await seed_gifts()
+
+    print("Bot ishga tushdi...")
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
